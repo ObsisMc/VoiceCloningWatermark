239e25ddce4615c6c077c9ae3727f5f1266b0afe
zrh_datalab <11912614@mail.sustech.edu.cn>
2023-12-09 12:10:23 -0600

    update scripts

--- scripts/run_trainVC.sh
+++ scripts/run_trainVC.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 beta=1
-lam=100
+lam=10000
 dtw=false
-lr=0.00005
+lr=0.00001
 val_itvl=500
 val_size=50
-num_epochs=30
+num_epochs=50
 batch_size=16
 experiment=1
-cuda_n=6
-summary="IDwl2lr1e-4audioMSElam100"
-transform="ID"
+cuda_n=4
+summary="VCwl2lr1e-5lam1e4"
+transform="VC"
 num_points=48000
 n_fft=1000
 hop_length=800
 num_layers=4
 watermark_len=2
 shift_ratio=0
-from_checkpoint=false
+from_checkpoint="1-IDwl2lr1e-4audioMSElam100/14-1-IDwl2lr1e-4audioMSElam100.pt"
 permutation=false
 stft_small=true
 ft_container="mag"
@@ -71,7 +71,7 @@ Epochs: $num_epochs
 val_itvl: $val_itvl its
 val_size: $val_size 
 
-CUDA_VISIBLE_DEVICES=X,Y python3 $ROOT/src/main.py --beta $beta --lam $lam --dtw $dtw --lr $lr --val_itvl $val_itvl --val_size $val_size --num_epochs $num_epochs --batch_size $batch_size --summary $summary --experiment $experiment --from_checkpoint $from_checkpoint --permutation $permutation --transform $transform --stft_small $stft_small --ft_container $ft_container --thet $thet --mp_encoder $mp_encoder --mp_decoder $mp_decoder --mp_join $mp_join --embed $embed --luma $luma --num_points $num_points --n_fft $n_fft --hop_length $hop_length --num_layers $num_layers --watermark_len $watermark_len --shift_ratio $shift_ratio
+CUDA_VISIBLE_DEVICES=X,Y python3 -u $ROOT/src/vc/main_vc.py --beta $beta --lam $lam --dtw $dtw --lr $lr --val_itvl $val_itvl --val_size $val_size --num_epochs $num_epochs --batch_size $batch_size --summary $summary --experiment $experiment --from_checkpoint $from_checkpoint --permutation $permutation --transform $transform --stft_small $stft_small --ft_container $ft_container --thet $thet --mp_encoder $mp_encoder --mp_decoder $mp_decoder --mp_join $mp_join --embed $embed --luma $luma --num_points $num_points --n_fft $n_fft --hop_length $hop_length --num_layers $num_layers --watermark_len $watermark_len --shift_ratio $shift_ratio
 EOF
 
-CUDA_VISIBLE_DEVICES=$cuda_n python3 $ROOT/src/main.py --beta $beta --lam $lam --dtw $dtw --lr $lr --val_itvl $val_itvl --val_size $val_size --num_epochs $num_epochs --batch_size $batch_size --summary $summary --experiment $experiment --from_checkpoint $from_checkpoint --permutation $permutation --transform $transform --stft_small $stft_small --ft_container $ft_container --thet $thet --mp_encoder $mp_encoder --mp_decoder $mp_decoder --mp_join $mp_join --embed $embed --luma $luma --num_points $num_points --n_fft $n_fft --hop_length $hop_length --num_layers $num_layers --watermark_len $watermark_len --shift_ratio $shift_ratio
+CUDA_VISIBLE_DEVICES=$cuda_n python3 -u $ROOT/src/vc/main_vc.py --beta $beta --lam $lam --dtw $dtw --lr $lr --val_itvl $val_itvl --val_size $val_size --num_epochs $num_epochs --batch_size $batch_size --summary $summary --experiment $experiment --from_checkpoint $from_checkpoint --permutation $permutation --transform $transform --stft_small $stft_small --ft_container $ft_container --thet $thet --mp_encoder $mp_encoder --mp_decoder $mp_decoder --mp_join $mp_join --embed $embed --luma $luma --num_points $num_points --n_fft $n_fft --hop_length $hop_length --num_layers $num_layers --watermark_len $watermark_len --shift_ratio $shift_ratio
